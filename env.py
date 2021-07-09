e64b009d91d05bed9f84fb2600618b2f73664ad1
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2021-07-09 12:15:29 +0100

    Mongo Project Updates

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@cluster0.gwvhn.mongodb.net/task_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r99tUser@cluster0.gwvhn.mongodb.net/task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "vICTORIApARK")
+os.environ.setdefault("SECRET_KEY", "fOUNDRYwOOD")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
